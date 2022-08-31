47883d8744f69f8711e92940f8760375a7563c18
Destman88 <derickmaitland@gmail.com>
2022-08-31 00:06:52 -0600

    Se agrega ejercicio 2 Sintaxis Js

---
+++ Ejercicio02-Sintaxis/index.js
@@ -0,0 +1,12 @@
+const info = [
+  "Derick",
+  34,
+  true,
+  new Date(1988, 6, 8),
+  {
+    titulo: "Viaje al Reino de los deseos",
+    autor: "Rafael Ángel Herra",
+    fecha: "1992",
+    url: "https://www.amazon.com/-/es/Rafael-Angel-Herra/dp/9977671729/ref=sr_1_1?adid=082VK13VJJCZTQYGWWCZ&campaign=211041&creative=374001&keywords=Viaje+Al+Reino+De+Los+Deseos&qid=1661924742&s=books&sr=1-1",
+  },
+];
